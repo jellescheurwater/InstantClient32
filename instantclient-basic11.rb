8f794f58b184d00e806c15174aefdd265c0a4eaa
Jelle Scheurwater <j.scheurwater@timeff.com>
2018-05-05 08:54:21 +0200

    Create instantclient-basic11.rb

---
+++ instantclient-basic11.rb
@@ -0,0 +1,26 @@
+require File.expand_path("../../Strategies/cache_wo_download", __FILE__)
+
+# A formula that installs the Instant Client Basic package.
+class InstantclientBasic11 < Formula
+  desc "Oracle Instant Client Basic x32"
+  homepage "http://www.oracle.com/technetwork/topics/intel-macsoft-096467.html"
+  hp = homepage
+
+  url "http://download.oracle.com/otn/mac/instantclient/11204/instantclient-basic-macos.x32-11.2.0.4.0.zip",
+      :using => (Class.new(CacheWoDownloadStrategy) do
+                   define_method :homepage do
+                     hp
+                   end
+                 end)
+  sha256 "6c079713ab0a65193f7bfcbad6c90e7806fa6634a3828052f8428e1533bb89d3"
+
+  conflicts_with "instantclient-basiclite",
+                 :because => "Differing versions of same formula"
+
+  def install
+    %w[libclntsh.dylib libocci.dylib].each do |dylib|
+      ln_s "#{dylib}.11.1", dylib
+    end
+    lib.install Dir["*.dylib*"]
+  end
+end
